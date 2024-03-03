75122175b3d0a931b363230d7f2322fcbc0c5b06
محمد الصوالحي <ms.2052001@gmail.com>
2024-03-03 14:21:12 +0200

    Solve problem_solving_hard

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from repo.Solvers.fox_submission_solver import init_fox
+from repo.Solvers.riddle_solver import problem_solving_hard
 # Example usage
 # team_id='ds42W0d'
 team_id = "your_team_id"
-message = init_fox(team_id)
-print("Message:", message)
+# message = init_fox(team_id)
+# print("Message:", message)
+
+m,n = map(int, input("m n: ").split())
+print(problem_solving_hard(m,n))
--- repo/Solvers/riddle_solvers.py
+++ repo/Solvers/riddle_solvers.py
@@ -178,7 +178,13 @@ def solve_problem_solving_hard(input: tuple) -> int:
     Returns:
     int: An integer representing the solution to the problem.
     """
-    return 0
+    m, n = input
+    dp = [[0]*(n + 1) for i in range(m + 1)]
+    dp[1][1] = 1
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+    return dp[m][n]
 
 
 riddle_solvers = {
