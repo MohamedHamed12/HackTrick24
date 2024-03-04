ffa014af3d2bacbe5839343859de67acc3a932e4
MohamedHamed12 <mh1779371@gmail.com>
2024-03-04 16:29:36 +0200

    Solve sec_hard

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 from repo.LSBSteg import decode
 from repo.Solvers.fox_submission_solver import generate_message_array, submit_fox_attempt
 from repo.Solvers.fox_utils import split_massage_chunks
-from repo.Solvers.riddle_solvers import solve_problem_solving_hard, solve_problem_solving_medium
+from repo.Solvers.riddle_solvers import solve_problem_solving_hard, solve_problem_solving_medium, solve_sec_hard
 
 api_base_url = "http://13.53.169.72:5000"
 
@@ -21,6 +21,12 @@ def riddle_ps_medium():
 def riddle_ps_hard():
     m, n = map(int, input("m n: ").split())
     print(solve_problem_solving_hard([m, n]))
+
+
+def riddle_sec_hard():
+    key = "266200199BBCDFF1"
+    plain_text = "0123456789ABCDEF"
+    print(solve_sec_hard([key, plain_text]))
 
 
 def get_remaining_attempts():
@@ -44,6 +50,5 @@ def encode_decode():
 
 
 get_remaining_attempts()
-# encode_decode()
 
-submit_fox_attempt(team_id)
+# submit_fox_attempt(team_id)
